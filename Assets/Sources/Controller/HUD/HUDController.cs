36d8da3f34720f2273825e9e1f99120af58fd56c
Dankisey <greeng3n@gmail.com>
2023-07-23 12:52:50 +0300

    HUDRedraw

--- Assets/Sources/Controller/HUD/HUDController.cs
+++ Assets/Sources/Controller/HUD/HUDController.cs
@@ -6,50 +6,12 @@ namespace Game.Controller
 {
     public class HUDController : MonoBehaviour
     {   
-        [SerializeField] private HUDPanel _mainPanel;
-
         private Battle _currentBattle;
         private World _world;
 
         public void Init(World world)
         {
             _world = world;
-            _world.BattleInitiated += OnBattleInitiated;
-        }
-
-        private void OnBattleInitiated(Battle battle)
-        {
-            _currentBattle = battle;
-            _currentBattle.Ended += OnBattleEnded;
-            _currentBattle.PlayerAttackRecieved += OnPlayerAttackRecieved;
-        }
-
-        private void OnBattleEnded()
-        {
-            _currentBattle.Ended -= OnBattleEnded;
-            _currentBattle.PlayerAttackRecieved -= OnPlayerAttackRecieved;
-        }
-
-        private void OnPlayerAttackRecieved()
-        {
-            _mainPanel.Open();
-        }
-
-        private void Start()
-        {
-            _mainPanel.Open();
-        }
-
-        private void OnEnable()
-        {
-            if (_world != null)
-                _world.BattleInitiated += OnBattleInitiated;
-        }
-
-        private void OnDisable()
-        {
-            if (_world != null)
-                _world.BattleInitiated -= OnBattleInitiated;
         }
     }
 }
